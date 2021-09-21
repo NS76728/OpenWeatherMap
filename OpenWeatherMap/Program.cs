915af6879b5a8de53537a300fa1aba6af7d6e046
Nick Stockton <phoenix76728@gmail.com>
2021-09-21 15:13:58 -0400

    initial

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace OpenWeatherMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+          
+            Console.WriteLine("Enter your API");
+            var key = Console.ReadLine();
+            bool stopper = true;
+            while (stopper)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter city name (Enter stop to exit)");
+                string city = Console.ReadLine();
+                if (city == "stop")
+                {
+                    stopper = false;
+                }
+                else
+                {
+                    var weatherUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={key}";
+                    var response = client.GetStringAsync(weatherUrl).Result;
+                    var formatresponse = JObject.Parse(response).GetValue("main").ToString();
+                    Console.WriteLine(formatresponse);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
